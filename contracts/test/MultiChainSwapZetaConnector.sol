65ac349e1254f8e54795310e478d2140511862cc
Kaustubh251002 <kmishra2510@gmail.com>
2023-02-21 15:24:57 +0530

    Debugged contracts

---
+++ contracts/test/MultiChainSwapZetaConnector.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@zetachain/protocol-contracts/contracts/interfaces/ZetaInterfaces.sol";
+import "@zetachain/protocol-contracts/contracts/interfaces/ZetaErrors.sol";
+
+import "../MultiChainSwap.sol";
+
+contract MultiChainSwapZetaConnector is ZetaConnector, ZetaErrors {
+    address public immutable zetaToken;
+
+    constructor(address zetaToken_) {
+        if (zetaToken_ == address(0)) revert InvalidAddress();
+        zetaToken = zetaToken_;
+    }
+
+    function callOnZetaMessage(
+        bytes memory zetaTxSenderAddress,
+        uint256 sourceChainId,
+        address destinationAddress,
+        uint256 zetaValueAndGas,
+        bytes calldata message
+    ) public {
+        return
+            MultiChainSwap(payable(destinationAddress)).onZetaMessage(
+                ZetaInterfaces.ZetaMessage({
+                    zetaTxSenderAddress: zetaTxSenderAddress,
+                    sourceChainId: sourceChainId,
+                    destinationAddress: destinationAddress,
+                    zetaValueAndGas: zetaValueAndGas,
+                    message: message
+                })
+            );
+    }
+
+    function callOnZetaRevert(
+        address zetaTxSenderAddress,
+        uint256 sourceChainId,
+        uint256 destinationChainId,
+        bytes calldata destinationAddress,
+        uint256 zetaValueAndGas,
+        uint256, // destinationGasLimit
+        bytes calldata message
+    ) public {
+        return
+            MultiChainSwap(payable(zetaTxSenderAddress)).onZetaRevert(
+                ZetaInterfaces.ZetaRevert({
+                    zetaTxSenderAddress: zetaTxSenderAddress,
+                    sourceChainId: sourceChainId,
+                    destinationAddress: destinationAddress,
+                    destinationChainId: destinationChainId,
+                    zetaValueAndGas: zetaValueAndGas,
+                    message: message
+                })
+            );
+    }
+
+    function send(ZetaInterfaces.SendInput calldata sendInput) external override {
+        uint256 sourceChainId = sendInput.destinationChainId == 2 ? 1 : 2;
+        address dest = address(uint160(bytes20(sendInput.destinationAddress)));
+
+        if (sendInput.zetaValueAndGas > 0) {
+            bool success = IERC20(zetaToken).transferFrom(msg.sender, dest, sendInput.zetaValueAndGas);
+            require(success == true, "MultiChainSwap: error transferring token");
+        }
+
+        return
+            callOnZetaMessage(
+                abi.encodePacked(msg.sender),
+                sourceChainId,
+                dest,
+                sendInput.zetaValueAndGas,
+                sendInput.message
+            );
+    }
+}
